f94347d9faad52d8f0f5476f22fd86cd26e122a2
Josh Flachsbart <josh.flachsbart@climate.com>
2022-12-02 22:36:07 -0600

    Fixed linter errors

--- day02/scoureCounter.py
+++ day02/scoureCounter.py
@@ -1,26 +1,25 @@
-shapeScore = { 'X':1, 'Y':2, 'Z':3 }
-winScore = { 'WIN': 6 , 'LOSE': 0 ,  'DRAW': 3 }
+shape_score = { 'X':1, 'Y':2, 'Z':3 }
+win_score = { 'WIN': 6 , 'LOSE': 0 ,  'DRAW': 3 }
 outcomes = { 'A X' : 'DRAW',
-	'A Y' : 'WIN',
-	'A Z' : 'LOSE',
-	'B X' : 'LOSE',
-	'B Y' : 'DRAW',
-	'B Z' : 'WIN',
-	'C X' : 'WIN',
-	'C Y' : 'LOSE',
-	'C Z' : 'DRAW' }
+    'A Y' : 'WIN',
+    'A Z' : 'LOSE',
+    'B X' : 'LOSE',
+    'B Y' : 'DRAW',
+    'B Z' : 'WIN',
+    'C X' : 'WIN',
+    'C Y' : 'LOSE',
+    'C Z' : 'DRAW' }
 
-def calcScore():
-	""" Calculate the total score from cheat sheet """
-	elfScore = 0
-	with open('data.txt') as dataFile:
-		done = False
-		while not done:
-			line = dataFile.readline().strip()
-			if not line:
-				done = True
-				break
-			elfScore += shapeScore[line.split()[1]]
-			elfScore += winScore[outcomes[line]]
-	return elfScore
-
+def calc_score():
+    """ Calculate the total score from cheat sheet """
+    elf_score = 0
+    with open('data.txt', encoding='utf-8') as data_file:
+        done = False
+        while not done:
+            line = data_file.readline().strip()
+            if not line:
+                done = True
+                break
+            elf_score += shape_score[line.split()[1]]
+            elf_score += win_score[outcomes[line]]
+    return elf_score
