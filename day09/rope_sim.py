b45c4a150f87b5dbed3dcc78694c723dd48b9134
Josh Flachsbart <josh.flachsbart@climate.com>
2022-12-09 00:55:50 -0600

    Streamlined part one to use the solution from part 2.  Day 9.

--- day09/rope_sim.py
+++ day09/rope_sim.py
@@ -1,31 +1,6 @@
 """ Module to simulate rope movement """
 
-visited_hash = {}
-visited_hash_2 = {}
-head_loc = [0,0]
-tail_loc = [0,0]
-knots = [[0,0] for i in range(10)]
-
 def move_tail(head_loc, tail_loc):
-    new_loc = [tail_loc[0], tail_loc[1]]
-    x_dist = head_loc[0] - tail_loc[0]
-    y_dist = head_loc[1] - tail_loc[1]
-    if abs(x_dist) <= 1 and abs(y_dist) <= 1:
-        new_loc = tail_loc
-    else:
-        if y_dist != 0:
-            if (y_dist > 0):
-                new_loc[1] += 1
-            elif (y_dist < 0):
-                new_loc[1] -= 1
-        if x_dist != 0:
-            if (x_dist > 0):
-                new_loc[0] += 1
-            elif (x_dist < 0):
-                new_loc[0] -= 1
-    return new_loc
-
-def move_tail_2(head_loc, tail_loc):
     x_dist = head_loc[0] - tail_loc[0]
     y_dist = head_loc[1] - tail_loc[1]
     if not (abs(x_dist) <= 1 and abs(y_dist) <= 1):
@@ -52,7 +27,7 @@ def test_tail():
 
 #test_tail()
 
-def move_head_2(command):
+def move_head(command, knots, visited_hash):
     (dir, dist) = command.split()
     head_loc = knots[0]
     # I WANNA USE MATCH.  Upgrate to python 3.10 or newer?
@@ -68,34 +43,14 @@ def move_head_2(command):
         
         prev = head_loc
         for knot in knots[1:]: 
-            move_tail_2(prev, knot)
+            move_tail(prev, knot)
             prev = knot
         
         tail_loc = knots[-1]
         loc_string = f'{tail_loc[0]},{tail_loc[1]}'
 #        print(loc_string)
         if loc_string not in visited_hash:
-            visited_hash_2[loc_string] = True
-
-def move_head(command):
-    (dir, dist) = command.split()
-    # I WANNA USE MATCH.  Upgrate to python 3.10 or newer?
-    for i in range(int(dist)):
-        if dir == 'U':
-            head_loc[1] += 1
-        elif dir == 'D':
-            head_loc[1] -= 1
-        elif dir == 'R':
-            head_loc[0] += 1
-        elif dir == 'L':
-            head_loc[0] -= 1
-
-        move_tail_2(head_loc, tail_loc)
-        loc_string = f'{tail_loc[0]},{tail_loc[1]}'
-#        print(loc_string)
-        if loc_string not in visited_hash:
             visited_hash[loc_string] = True
-
 
 def load_moves(file_name):
     """ Read the rope moves into array """
@@ -105,13 +60,17 @@ def load_moves(file_name):
     
     return all_lines
 
-
 def run():
-    for move in load_moves('data.txt'):
-#        move_head(move)
-        move_head_2(move)
+    visited_hash_1 = {}
+    visited_hash_2 = {}
+    knots_1 = [[0,0] for i in range(2)]
+    knots_2 = [[0,0] for i in range(10)]
 
-#    print ('Part 1 6337:', len(visited_hash))
-    print ('Part 2 36:', len(visited_hash_2))
+    for move in load_moves('data.txt'):
+        move_head(move, knots_1, visited_hash_1)
+        move_head(move, knots_2, visited_hash_2)
+
+    print ('Part 1 6337:', len(visited_hash_1))
+    print ('Part 2 2455:', len(visited_hash_2))
 
 run()
